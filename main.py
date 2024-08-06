e1ebee2af3dfeb5c8595f37c6983f798ccae7f1b
unknown <throwaway0x069420@gmail.com>
2024-08-06 03:26:06 -0400

    updated resource_server_configuration and enabled testing tokens

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from flask_cors import CORS, cross_origin
 
 
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -25,6 +26,12 @@ def closeOffLog(log):
         pass
         # during prod we're not actually going to log anything
 
+closeOffLog(f"""==== STARTUP AT {datetime.now()} ====
+GP Accessible Files: {str(cfg.acceptable_files)}
+Port #: {cfg.port}
+==== END STARTUP LOG ====""")
+
+
 @app.route('/resource', methods=["GET", "OPTIONS"])
 @cross_origin()
 def resource():
@@ -36,7 +43,7 @@ def resource():
         return response
 
     toLog = f"{request.remote_addr}@{str(datetime.now())} "
-    
+
     # First, we are going to check if the Auth. header is right
     token = request.headers.get('Authorization')
     if token == None or len(token.split(" ")) != 2 or token.split(" ")[0] != "Bearer":
@@ -45,27 +52,37 @@ def resource():
         return Response("401 UNAUTHORIZED: Missing or malformed Authorization header (must be a Bearer token).", status=401, mimetype='text/plain')
 
     # Second, we are going to check if the user that called for the issuance of this token is a member of MCPSMD
-    url = "https://graph.microsoft.com/v1.0/me"
-    headers = {
-        'Authorization': 'Bearer ' + token.split(" ")[1],
-        'Content-Type': 'application/json'
-    }
-    
-    response = http.get(url, headers=headers)
-    
-    if response.status_code == 401:
-        toLog = toLog + " -> 401:Bad Token"
+    if token.split(" ")[1].startswith("testing:") and token.split(" ")[1] in cfg.db_acs:
+        toLog = toLog + " <accessed via " + token.split(" ")[1] + ", no UPN available> "
+    elif token.split(" ")[1].startswith("testing:") and token.split(" ")[1] not in cfg.db_acs:
+        toLog = toLog + " -> 401:Bad Token (testing token used)"
         closeOffLog(toLog)
-        return Response("401 UNAUTHORIZED: Access token does not correspond to any user, was tampered with, has expired or is otherwise invalid.", status=401, mimetype='text/plain')
+        return Response("401 UNAUTHORIZED: testing:* token provided was not correct.", status=401, mimetype='text/plain')
 
-    userPrincipalName = json.loads(response.text)["userPrincipalName"]
-
-    if userPrincipalName.lower().endswith("mcpsmd.org") == False:
-        toLog = toLog + f": {userPrincipalName.lower()} -> 401:Non MCPSMD"
-        closeOffLog(toLog)
-        return Response("401 UNAUTHORIZED: The user behind this token is not an MCPSMD user.", status=401, mimetype='text/plain')
     else:
-        toLog = toLog + f": {userPrincipalName.lower()} "
+        url = "https://graph.microsoft.com/v1.0/me"
+        headers = {
+            'Authorization': 'Bearer ' + token.split(" ")[1],
+            'Content-Type': 'application/json'
+        }
+
+        response = http.get(url, headers=headers)
+        elapsed = response.elapsed.total_seconds()
+        toLog = toLog + " (AAD verify time " + str(elapsed) + "s) "
+
+        if response.status_code == 401:
+            toLog = toLog + " -> 401:Bad Token"
+            closeOffLog(toLog)
+            return Response("401 UNAUTHORIZED: Azure AD Access token does not correspond to any user, was tampered with, has expired or is otherwise invalid.", status=401, mimetype='text/plain')
+
+        userPrincipalName = json.loads(response.text)["userPrincipalName"]
+
+        if userPrincipalName.lower().endswith("mcpsmd.org") == False:
+            toLog = toLog + f": {userPrincipalName.lower()} -> 401:Non MCPSMD"
+            closeOffLog(toLog)
+            return Response("401 UNAUTHORIZED: The Azure AD user behind this token is not an MCPSMD user.", status=401, mimetype='text/plain')
+        else:
+            toLog = toLog + f": {userPrincipalName.lower()} "
 
     # Third, we are going to check if the file that the user has called for exists, and that they are allowed to access it
     # (we aren't going to let users call for any file on the fs willy nilly - they can only pull from a premade list)
@@ -85,20 +102,13 @@ def resource():
         closeOffLog(toLog)
         return Response(f"""403 FORBIDDEN: This user does not have permission to read this file ({request.args.get('file')})""", status=403, mimetype='text/plain')
         # There is supposed to be a check here where a specific user who has been given special permission to read a certain file
-        # But we aren't there yet 
+        # But we aren't there yet
 
     # We only get to this spot if all conditions were met
     toLog = toLog + f": -> 200:OK"
     closeOffLog(toLog)
     return send_file(cfg.acceptable_files[file_number], cfg.mime_types[file_number])
-    
-    
 
-    
 
-if __name__ == '__main__':
-    closeOffLog(f"""==== STARTUP AT {datetime.now()} ====
-GP Accessible Files: {str(cfg.acceptable_files)}
-Port #: {cfg.port}
-==== END STARTUP LOG ====""")
-    app.run(port=cfg.port)
+
+
--- resource_server_configuration.py
+++ resource_server_configuration.py
@@ -1,7 +1,8 @@
-acceptable_files = ['all_map_data.txt', 'map_1f.jpg', 'map_2f.jpg', 'map.jpg']
-mime_types = ['application/json', 'image/jpeg', 'image/jpeg', 'image/jpeg']
+acceptable_files = ['all_map_data.txt', 'map_1f.jpg', 'map_2f.jpg', 'map.jpg', 'map_wc.jpg', 'placeholder.jpg', 'map_aer.jpg', '2.0_phs_allmap_dev.txt']
+mime_types = ['application/json', 'image/jpeg', 'image/jpeg', 'image/jpeg', 'image/jpeg', 'image/jpeg', 'image/jpeg', 'text/plain']
 port = 8081
-doLog = False
+doLog = True
+db_acs = []
 
 class User:
     def __init__(self, write = [], read = [], create = False, delete = False):
